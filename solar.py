532aa8dd54a4aa84d2423b0195a1120a20e9221e
dpelletierLM <donald.pelletier@libertymutual.com>
2022-06-07 11:29:49 -0400

    Updating Astronomy Lab

--- solar.py
+++ solar.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 import requests
-import datetime
 import json
+from datetime import datetime, date
+timenow = datetime.now()
+current_time = timenow.strftime("%H:%M:%S")
+
 
 ASTRONOMYAPI_ID = "5afae862-243b-477b-bcf0-400cc33e5a1b"
 ASTRONOMYAPI_SECRET = "14ced2f1c9212a9ccdd014529be2c8e187c870889b8a28ca809b1162e24e77acd92664198e775dce7e7dcf8838f7c80ed340abb2415086ebbf5c359546099454a8dc2243e62a76dda64816be192d153f09128ba455ab5f2592b4478a28affec27a5e7ce24a1db719f04b76e7adfbf222"
@@ -11,13 +14,27 @@ def get_observer_location():
     str: latitude
     str: longitude
     """
-    response = requests.get("http://ip-api.com/json/18.206.10.123?fields=lat,lon")
-    lat_lon = json.loads(response.text)
+    location_url = "http://ip-api.com/json?fields=lat,lon"
+    response = requests.get(location_url)
+    #response = requests.get('http://ip-api.com/json/18.206.10.123?fields=lat,lon')
+    lat_lon = response.json()
     latitude = lat_lon['lat']
     longitude = lat_lon['lon']
 
     # NOTE: Replace with your real return values!
+    print(latitude,longitude)
     return latitude, longitude
+
+def get_elevation(latitude,longitude): # Gets elevation from latitude and longitude 
+    elev_url = "https://api.open-elevation.com/api/v1/lookup?locations={0},{1}".format(
+        latitude,
+        longitude,
+        )
+    elev_response = requests.get(elev_url)
+    elev_response_data = elev_response.json()
+    elevation = elev_response_data["results"][0]["elevation"]
+    print(elevation)
+    return elevation
     
 def get_sun_position(latitude, longitude):
     """Returns the current position of the sun in the sky at the specified location
@@ -34,6 +51,9 @@ def get_sun_position(latitude, longitude):
     response = requests.get('https://api.astronomyapi.com/api/v2/bodies/', auth=(ASTRONOMYAPI_ID, ASTRONOMYAPI_SECRET))
     print(response)
 
+    #Get all available bodies
+    # 
+
     # NOTE: Replace with your real return values!
     return 0, 0
         
@@ -49,5 +69,6 @@ def print_position(azimuth, altitude):
     
 if __name__== "__main__":
     latitude, longitude = get_observer_location()
+    elevation = get_elevation(latitude,longitude)
     azimuth, altitude = get_sun_position(latitude, longitude)
     print_position(azimuth, altitude)
